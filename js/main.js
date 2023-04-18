1831c34ad168c396adddf80bf4679404b4b7fb7f
Jackson <yinelona@gmail.com>
2023-04-18 07:43:09 -0700

    WorldLight_2023_04_18_Pixel-Perfect_Update

--- js/main.js
+++ js/main.js
@@ -94,20 +94,31 @@ $(document).ready(function(){
         speed: 800,
         rtl: false,
         cssEase: 'linear',
-        slidesToShow: 5,
+        slidesToShow: 6,
+        slidesToScroll: 6,
         responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    infinite: true,
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                },
+            },
             {
                 breakpoint: 768,
                 settings: {
-                    infinite: false,
+                    infinite: true,
                     slidesToShow: 3,
+                    slidesToScroll: 3,
                 },
             },
             {
                 breakpoint: 480,
                 settings: {
-                    infinite: false,
+                    infinite: true,
                     slidesToShow: 2,
+                    slidesToScroll: 2,
                 },
             },
         ],
